c6d1bf0292694e6ae6bacac306d8c36005fdb8ed
5220genngxueyu <535754356@qq.com>
2021-06-09 19:57:59 +0800

    3-4 集成mybatis generator 生成demo表代码+demo一些类

---
+++ wiki/src/main/java/com/jiava/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.jiava.wiki.service;
+
+import com.jiava.wiki.domain.Demo;
+import com.jiava.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list(){
+        return demoMapper.selectByExample(null);
+    }
+}
