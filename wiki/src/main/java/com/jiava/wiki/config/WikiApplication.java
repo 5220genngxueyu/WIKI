7dd22abef3c53c76af9ddef806d23e4fd9ca7a2c
5220genngxueyu <535754356@qq.com>
2021-06-09 09:24:52 +0800

    2-6挪动包，增加@ComponentScan注解

--- wiki/src/main/java/com/jiava/wiki/config/WikiApplication.java
+++ wiki/src/main/java/com/jiava/wiki/config/WikiApplication.java
@@ -9,6 +9,7 @@ import org.springframework.core.env.Environment;
 
 //下面这个注解只是会扫描它所在的包下的子包，其他的就访问不到了
 //下面如果要访问其他包，要加@ComponentScan
+//扫描多个包的写法@ComponentScan({"com.jiava","com.test"})
 @ComponentScan("com.jiava")
 @SpringBootApplication
 public class WikiApplication {
