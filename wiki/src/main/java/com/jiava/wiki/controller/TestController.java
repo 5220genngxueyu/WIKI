4f4f47308d44c2fd35f62406558a6d1095e39d17
5220genngxueyu <535754356@qq.com>
2021-06-09 10:21:47 +0800

    2-8 增加了自定义配置项示例

--- wiki/src/main/java/com/jiava/wiki/controller/TestController.java
+++ wiki/src/main/java/com/jiava/wiki/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.jiava.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -7,13 +9,15 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
     //支持所有请求方式
     //只支持get方法的写法
     // @RequestMapping(value = "/hello",method = RequestMethod.GET)
     //@RequestMapping( "/hello")
     @GetMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world"+testHello;
     }
     @PostMapping("/hello/post")
     public String hello(String name){
