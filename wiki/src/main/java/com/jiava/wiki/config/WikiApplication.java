fc5a193515c67aaa49ea8a21aabaea508941de97
5220genngxueyu <535754356@qq.com>
2021-06-09 09:21:25 +0800

    2-6挪动包，增加@ComponentScan注解

--- wiki/src/main/java/com/jiava/wiki/config/WikiApplication.java
+++ wiki/src/main/java/com/jiava/wiki/config/WikiApplication.java
@@ -1,12 +1,15 @@
-package com.jiava.wiki;
+package com.jiava.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 //下面这个注解只是会扫描它所在的包下的子包，其他的就访问不到了
+//下面如果要访问其他包，要加@ComponentScan
+@ComponentScan("com.jiava")
 @SpringBootApplication
 public class WikiApplication {
 
