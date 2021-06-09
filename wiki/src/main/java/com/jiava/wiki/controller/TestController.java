b0be3bd94a9046b556da6f6d62cb88d802521265
5220genngxueyu <535754356@qq.com>
2021-06-09 09:43:17 +0800

    2-7 添加HTTP测试文件

--- wiki/src/main/java/com/jiava/wiki/controller/TestController.java
+++ wiki/src/main/java/com/jiava/wiki/controller/TestController.java
@@ -1,8 +1,8 @@
 package com.jiava.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class TestController {
@@ -10,8 +10,13 @@ public class TestController {
     //支持所有请求方式
     //只支持get方法的写法
     // @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    @RequestMapping( "/hello")
+    //@RequestMapping( "/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+    @PostMapping("/hello/post")
+    public String hello(String name){
+        return "hello world! Post"+name;
     }
 }
