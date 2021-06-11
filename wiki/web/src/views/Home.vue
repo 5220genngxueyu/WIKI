b826584ae0a300497d92dbd26c2854d3b61619ce
5220genngxueyu <535754356@qq.com>
2021-06-11 14:09:05 +0800

    5-2 使用ref,reactive实现数据绑定

--- wiki/web/src/views/Home.vue
+++ wiki/web/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout>
-  <a-layout-sider width="200" style="background: #fff">
+    <a-layout-sider width="200" style="background: #fff">
       <a-menu
           mode="inline"
           v-model:selectedKeys="selectedKeys2"
@@ -45,24 +45,42 @@
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
-  <a-layout-content
+    <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      Content
+    <pre>
+{{ ebooks }}
+{{ ebooks2 }}
+    </pre>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
-import  axios from 'axios'
+import {defineComponent, onMounted, ref, reactive,toRef} from 'vue';
+import axios from 'axios'
+
 export default defineComponent({
   name: 'Home',
-  setup(){
+  setup() {
     console.log("setup");
-    axios.get("http://localhost:8089/ebook/list?name=Spring").then((response)=>{
-      console.log(response);
+    const ebooks = ref();
+    const ebooks1 = reactive({books: []});
+
+    onMounted(function () {
+      console.log("onMounted");
+      axios.get("http://localhost:8089/ebook/list?name=Spring").then((response) => {
+        const data = response.data;
+        ebooks.value = data.content;
+        ebooks1.books = data.content;
+        console.log(response);
+
+      });
     })
+    return {
+      ebooks,
+      ebooks2 : toRef(ebooks1,"books")
+    }
   }
 });
 </script>
