6f51fbf41f27d7c20f1ec6c7b8aafaa5663dd0ed
5220genngxueyu <535754356@qq.com>
2021-06-17 16:34:03 +0800

    8-2 增加文档表SQL，并生成持久层代码

--- wiki/doc/all.sql
+++ wiki/doc/all.sql
@@ -45,28 +45,28 @@ insert into `ebook` (id,name,description)
 values (4,'Mysql 入门教程','零基础入门Mysql开发，企业及应用开发最佳首选框架');
 insert into `ebook` (id,name,description)
 values (5,'Oracle 入门教程','零基础入门Oracle开发，企业及应用开发最佳首选框架');
-#分类
-drop table if exists `ebook`;
-create table `ebook`(
+#文档
+drop table if exists `doc`;
+create table `doc`(
                         `id` bigint not null comment 'id',
+                        `parent` bigint not null default 0 comment '父id',
+                        `ebook_id` bigint not null default 0 comment '电子书id',
                         `name` varchar(50) comment '名称',
-                        `category1_id` bigint comment '分类1',
-                        `category2_id` bigint comment '分类2',
-                        `description` varchar(50) comment '描述',
-                        `cover` varchar(50) comment '封面',
-                        `doc_count` int comment '文档数',
+                        `sort` int comment '顺序',
                         `view_count` int comment '阅读数',
                         `vote_count` int comment '点赞数',
                         primary key (`id`)
-)engine = innodb default charset =utf8mb4 comment ='电子书';
+)engine = innodb default charset =utf8mb4 comment ='文档';
 
-insert into `ebook` (id,name,description)
-values (1,'Spring Boot 入门教程','零基础入门Java开发，企业及应用开发最佳首选框架');
-insert into `ebook` (id,name,description)
-values (2,'Vue 入门教程','零基础入门Vue开发，企业及应用开发最佳首选框架');
-insert into `ebook` (id,name,description)
-values (3,'python 入门教程','零基础入门Python开发，企业及应用开发最佳首选框架');
-insert into `ebook` (id,name,description)
-values (4,'Mysql 入门教程','零基础入门Mysql开发，企业及应用开发最佳首选框架');
-insert into `ebook` (id,name,description)
-values (5,'Oracle 入门教程','零基础入门Oracle开发，企业及应用开发最佳首选框架');
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count)
+values (1,1,0,'文档1',1,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count)
+values (2,1,1,'文档1.1',1,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count)
+values (3,1,0,'文档2',2,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count)
+values (4,1,3,'文档2.1',1,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count)
+values (5,1,3,'文档2.2',2,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count)
+values (6,1,5,'文档2.2.1',1,0,0);
