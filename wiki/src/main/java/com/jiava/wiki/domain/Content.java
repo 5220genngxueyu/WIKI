e16540f16ce447a48ffe38eb9a54cdb51b694131
5220genngxueyu <535754356@qq.com>
2021-06-18 15:49:35 +0800

    8-8 文档内容表的设计和代码生成

---
+++ wiki/src/main/java/com/jiava/wiki/domain/Content.java
@@ -0,0 +1,39 @@
+package com.jiava.wiki.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.jiava.wiki.util.JsonLongSerializer;
+
+public class Content {
+    @JsonSerialize(using = JsonLongSerializer.class )
+    private Long id;
+
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", content=").append(content);
+        sb.append("]");
+        return sb.toString();
+    }
+}
