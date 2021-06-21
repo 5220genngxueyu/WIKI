bc5531afb0a8ef817d9a873ad4771ccdf4cad7af
5220genngxueyu <535754356@qq.com>
2021-06-21 15:56:05 +0800

    9-5 密码的加密传输和加密存储

--- wiki/src/main/java/com/jiava/wiki/controller/UserController.java
+++ wiki/src/main/java/com/jiava/wiki/controller/UserController.java
@@ -6,10 +6,12 @@ import com.jiava.wiki.resp.CommonResp;
 import com.jiava.wiki.resp.UserQueryResp;
 import com.jiava.wiki.resp.PageResp;
 import com.jiava.wiki.service.UserService;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/user")
@@ -19,6 +21,7 @@ public class UserController {
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req){
+
         CommonResp<PageResp<UserQueryResp> > resp = new CommonResp<>();
         PageResp<UserQueryResp> list=userService.list(req);
         resp.setContent(list);
@@ -27,6 +30,7 @@ public class UserController {
 
     @PostMapping("/save")
     public CommonResp save(@Valid @RequestBody UserSaveReq req){
+        req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp resp = new CommonResp<>();
         userService.save(req);
         return resp;
--- wiki/web/src/views/admin/admin-user.vue
+++ wiki/web/src/views/admin/admin-user.vue
@@ -158,7 +158,7 @@ export default defineComponent({
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
-
+      user.value.password = hexMd5(user.value.password + KEY);
 
       axios.post("/user/save", user.value).then((response) => {
         modalLoading.value = false;
