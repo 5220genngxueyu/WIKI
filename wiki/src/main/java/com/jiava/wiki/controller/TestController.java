7e27f9ac77451fb691340b754465dcd11c22749b
5220genngxueyu <535754356@qq.com>
2021-06-21 19:10:53 +0800

    9-9 增加redis测试接口

--- wiki/src/main/java/com/jiava/wiki/controller/TestController.java
+++ wiki/src/main/java/com/jiava/wiki/controller/TestController.java
@@ -2,19 +2,26 @@ package com.jiava.wiki.controller;
 
 import com.jiava.wiki.domain.Test;
 import com.jiava.wiki.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
     @Resource
     private TestService testService;
-
+    @Resource
+    private RedisTemplate redisTemplate;
     @Value("${test.hello:TEST}")
     private String testHello;
     //支持所有请求方式
@@ -32,5 +39,19 @@ public class TestController {
     @GetMapping("/test/list")
     public List<Test> list(){
         return testService.list();
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
