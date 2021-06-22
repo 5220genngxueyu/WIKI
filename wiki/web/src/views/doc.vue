2762a1c44e7451d143e6a6c3f19624a4fa038eed
5220genngxueyu <535754356@qq.com>
2021-06-22 12:51:11 +0800

    10-2 文档页面上显示阅读数和点赞数

--- wiki/web/src/views/doc.vue
+++ wiki/web/src/views/doc.vue
@@ -15,6 +15,14 @@
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div>
+            <h2>{{doc.name}}</h2>
+            <div>
+              <span>阅读数：{{doc.viewCount}}</span> &nbsp; &nbsp;
+              <span>点赞数：{{doc.voteCount}}</span>
+            </div>
+            <a-divider style="height: 2px; background-color: #9999cc"/>
+          </div>
           <div class="wangeditor" :innerHTML="html"></div>
         </a-col>
       </a-row>
@@ -36,7 +44,9 @@ export default defineComponent({
     const html = ref()
     const defaultSelectedKeys = ref();
     defaultSelectedKeys.value = [];
-
+// 当前选中的文档
+    const doc = ref();
+    doc.value = {};
     /**
      * 一级文档树，children属性就是二级文档
      * [{
@@ -67,6 +77,7 @@ export default defineComponent({
     const onSelect = (selectedKeys: any, info: any) => {
       console.log('selected', selectedKeys, info);
       if (Tool.isNotEmpty(selectedKeys)) {
+        doc.value= info.selectedNodes[0].props;
         // 加载内容
         handleQueryContent(selectedKeys[0]);
       }
@@ -90,6 +101,7 @@ export default defineComponent({
           if (Tool.isNotEmpty(level1)) {
             defaultSelectedKeys.value = [level1.value[0].id];
             handleQueryContent(level1.value[0].id);
+            doc.value=level1.value[0];
           }
         } else {
           message.error(data.message);
@@ -107,6 +119,7 @@ export default defineComponent({
       html,
       onSelect,
       defaultSelectedKeys,
+      doc,
     }
   }
 });
