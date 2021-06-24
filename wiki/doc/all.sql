fc543e9cb5f3bc3cadbfcefac344676df8d15a7c
5220genngxueyu <535754356@qq.com>
2021-06-24 17:26:29 +0800

    11-1 增加电子书快照表，生成持久层代码

--- wiki/doc/all.sql
+++ wiki/doc/all.sql
@@ -34,6 +34,18 @@ create table `ebook`(
     `vote_count` int comment '点赞数',
     primary key (`id`)
 )engine = innodb default charset =utf8mb4 comment ='电子书';
+#电子书快照表
+drop table if exists `ebook_snapshot`;
+create table `ebook_snapshot`(
+                        `id` bigint not null comment 'id',
+                        `ebook_id` varchar(50) comment '电子书id',
+                        `date` date not null comment '快照日期',
+                        `view_count` int comment '阅读数',
+                        `vote_count` int comment '点赞数',
+                        `view_increase` int comment '阅读增长',
+                        `vote_increase` int comment '点赞增长',
+                        primary key (`id`)
+)engine = innodb default charset =utf8mb4 comment ='电子书快照表';
 
 insert into `ebook` (id,name,description)
 values (1,'Spring Boot 入门教程','零基础入门Java开发，企业及应用开发最佳首选框架');
